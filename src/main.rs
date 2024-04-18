88032fe79ce8314961cb0c41db2f87af32bae06b
SL91994 <320sl91994@gmail.com>
2024-04-18 12:58:44 +0900

    closed #1 add:✨ Added video download function.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use rusty_ytdl::{Video, VideoOptions, VideoQuality, VideoSearchOptions};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please provide a video URL as a command line argument.");
+        return;
+    }
+
+    let video_url = &args[1];
+
+    let video_options = VideoOptions {
+        quality: VideoQuality::Highest,
+        filter: VideoSearchOptions::VideoAudio,
+        ..Default::default()
+    };
+
+    // BadApple
+    let video = Video::new_with_options(&*video_url, video_options).unwrap();
+
+    let stream = video.stream().await.unwrap();
+
+    while let Some(chunk) = stream.chunk().await.unwrap() {
+        println!("{:#?}", chunk);
+    }
+
+    // let music_path = std::path::Path::new(r"badapple.mp3");
+    // video.download(music_path).await.unwrap();
+    let video_path = std::path::Path::new(r"badapple.mkv");
+    video.download(video_path).await.unwrap();
 }

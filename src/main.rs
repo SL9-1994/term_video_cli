21427e99038a30a01dd3eb465b35c41b559a8e21
SL91994 <320sl91994@gmail.com>
2024-05-06 23:46:21 +0900

    add:✨ Command line argument parser with clap.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,42 @@
-use rusty_ytdl::{Video, VideoOptions, VideoQuality, VideoSearchOptions};
-use std::env;
+use clap::Parser;
+use std::{env::current_dir, path::PathBuf};
+use terminal::Terminal;
+use video_processing::VideoProcessor;
+mod downloder;
+mod terminal;
+mod video_processing;
+use crate::downloder::yt_video_downloader;
+#[derive(Parser)]
+#[command(version, about = "This is the terminal_ascii_video drawing CLI with video download capability.", long_about = None)]
+struct Cli {
+    /// Specify any YouTube URL to download the video.
+    #[arg(short, long, value_name = "url")]
+    url: Option<String>,
+
+    /// Enter the path of the video you wish to convert. (Supported extensions: mp4, mkv)
+    #[arg(short, long, value_name = "FILE_PATH")]
+    file_path: Option<PathBuf>,
+}
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Please provide a video URL as a command line argument.");
-        return;
+    let cli = Cli::parse();
+
+    let mut current_dir: PathBuf = current_dir().unwrap();
+    if let Some(url) = cli.url.as_deref() {
+        let local_video_path = yt_video_downloader(url.to_string()).await;
+        current_dir.push(local_video_path);
+
+        println!("The downloaded video path is {:?}", current_dir);
     }
 
-    let video_url = &args[1];
+    if let Some(file_path) = cli.file_path.as_deref() {
+        let tmp_dir = current_dir.join("tmp");
+        let processor = VideoProcessor::new(file_path.to_path_buf(), tmp_dir);
+        processor.convert_to_grayscale_and_frame().await;
 
-    let video_options = VideoOptions {
-        quality: VideoQuality::Highest,
-        filter: VideoSearchOptions::VideoAudio,
-        ..Default::default()
-    };
-
-    // BadApple
-    let video = Video::new_with_options(&*video_url, video_options).unwrap();
-
-    let stream = video.stream().await.unwrap();
-
-    while let Some(chunk) = stream.chunk().await.unwrap() {
-        println!("{:#?}", chunk);
+        let terminal = Terminal::new();
+        // ターミナルへasciiを出力する。
+        terminal.print_term_ascii().await;
     }
-
-    // let music_path = std::path::Path::new(r"badapple.mp3");
-    // video.download(music_path).await.unwrap();
-    let video_path = std::path::Path::new(r"badapple.mkv");
-    video.download(video_path).await.unwrap();
 }

0c68979037392fc94dd80ad74cc5a2685d66755c
yurari <320sl91994@gmail.com>
2025-05-07 22:42:31 +0900

    feat:✨ カスタムエラー型を追加し、エラーハンドリングを強化

---
+++ src/error.rs
@@ -0,0 +1,67 @@
+use std::path::PathBuf;
+
+use colored::Colorize;
+use thiserror::Error;
+
+#[macro_export]
+macro_rules! bail_on_error {
+    ($exit_code:expr) => {{
+        std::process::exit($exit_code);
+    }};
+}
+
+// トップレベルカスタムエラー
+#[non_exhaustive]
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("CLI> {0}")]
+    Cli(#[from] CliErr),
+}
+
+impl AppError {
+    pub fn pretty(&self) -> String {
+        format!("{}: {}", "[Error]".red(), self.to_string())
+    }
+}
+
+impl AppError {
+    pub fn exit_code(&self) -> i32 {
+        match self {
+            Self::Cli(e) => e.exit_code(),
+        }
+    }
+}
+
+#[non_exhaustive]
+#[derive(Error, Debug)]
+pub enum CliErr {
+    #[error("validation> {0}")]
+    Validation(#[from] ArgsValidationErr),
+}
+
+impl CliErr {
+    pub fn exit_code(&self) -> i32 {
+        match self {
+            Self::Validation(_) => 2,
+        }
+    }
+}
+
+#[non_exhaustive]
+#[derive(Error, Debug)]
+pub enum ArgsValidationErr {
+    #[error("Unsupported file extension: '{0}'. Supported types are: {1}")]
+    InvalidExtension(String, String),
+
+    #[error("Oops! The specified path doesn't exist: '{0}'")]
+    PathDoesNotExist(PathBuf),
+
+    #[error("Oops! The file extension does not exist: '{0}'")]
+    ExtNotExist(PathBuf),
+
+    #[error("Oops! The given path is not a file: '{0}'")]
+    PathIsNotFile(PathBuf),
+
+    #[error("Oops! It contains invalid character encoding, please change to UTF8 : {0}")]
+    InvalidCharCode(PathBuf),
+}

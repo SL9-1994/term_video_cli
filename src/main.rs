72738064e00a8ce7b66d5b657a174e669d621802
yurari <320sl91994@gmail.com>
2025-05-07 22:48:09 +0900

    refactor:♻️ リファクタリングし，エラー表示とハンドリングを最適化

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
-use clap::Parser;
+use cli::Cli;
+use error::AppError;
 use std::{env::current_dir, path::PathBuf};
 use terminal::Terminal;
 use video_processor::VideoProcessor;
 
+mod cli;
+mod error;
 mod terminal;
 mod video_processor;
 
-#[derive(Parser)]
-#[command(version, about = "This CLI can draw and play arbitrary videos and images to the terminal.", long_about = None)]
-struct Cli {
-    /// Enter the path of the video you wish to convert. (Supported extensions: mp4, mkv...)
-    /// Note: Since the conversion is based on the terminal size at the time this option is executed, a terminal of a different size will not be drawn correctly.
-    #[arg(short, long, value_name = "CONVERT_VIDEO_PATH")]
-    file_path: Option<PathBuf>,
+fn main() {
+    if let Err(error) = run() {
+        eprintln!("{}", error.pretty());
 
-    /// Enter the path of the image you want to convert. (Supported extensions: png, jpeg...)
-    /// Note: Terminals of different sizes will not render correctly as the conversion is based on the terminal size at the time this option is executed.
-    #[arg(short, long, value_name = "CONVERT_IMAGE_PATH")]
-    image_path: Option<PathBuf>,
+        let exit_code = error.exit_code();
 
-    /// Play ascii_video with the converted image already prepared in tmp.
-    #[arg(short, long)]
-    play: bool,
+        bail_on_error!(exit_code);
+    }
+}
+
+fn run() -> Result<(), AppError> {
+    let cli = Cli::new();
+    cli.validate()?;
+
+    if let Some(file_path) = cli.file_path {
+        process_video(&file_path.to_path_buf());
+    }
+
+    if let Some(image_path) = cli.image_path {
+        process_image(&image_path.to_path_buf());
+    }
+
+    if cli.play {
+        let terminal = Terminal::new();
+        let processor = VideoProcessor::new(None, None, &terminal);
+        let frames = processor.convert_to_ascii_art();
+        terminal.print_ascii_video(&frames);
+    }
+
+    Ok(())
 }
 
 ///  Performs the conversion process for the video of the given file path.
@@ -46,23 +63,4 @@ fn process_image(file_path: &PathBuf) {
     let tmp_dir = current_dir().unwrap().join("tmp");
     let v_processor = VideoProcessor::new(Some(file_path.to_path_buf()), Some(tmp_dir), &terminal);
     v_processor.convert_to_grayscale_and_resize();
-}
-
-fn main() {
-    let cli = Cli::parse();
-
-    if let Some(file_path) = cli.file_path {
-        process_video(&file_path.to_path_buf());
-    }
-
-    if let Some(image_path) = cli.image_path {
-        process_image(&image_path.to_path_buf());
-    }
-
-    if cli.play {
-        let terminal = Terminal::new();
-        let processor = VideoProcessor::new(None, None, &terminal);
-        let frames = processor.convert_to_ascii_art();
-        terminal.print_ascii_video(&frames);
-    }
 }
